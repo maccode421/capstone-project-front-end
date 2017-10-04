6c2b22a08dab5b48cf68e661819ffd0d34203956
maccode421 <maccavalli@yahoo.com>
2017-10-04 18:59:34 -0400

    create auth folder: set up ajax api calls

---
+++ assets/scripts/app.js
@@ -0,0 +1,5 @@
+'use strict'
+
+module.exports = {
+  host: 'https://tranquil-tundra-61203.herokuapp.com/'
+}
--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 const config = {
   apiOrigins: {
-    production: 'https://ga-wdi-boston.herokuapp.com'
+    production: 'https://tranquil-tundra-61203.herokuapp.com/'
   }
 }
 
